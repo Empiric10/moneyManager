b052f6f74bd81c5f19eeb4d14c3490c9b5bc65b4
Daniil Shlapak <Shlapak.Daniil@mail.ru>
2021-11-30 11:22:29 +0300

    project added

---
+++ MoneyManager/MoneyManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyManagerApp.swift
+//  MoneyManager
+//
+//  Created by Daniil Shlapak on 30.11.21.
+//
+
+import SwiftUI
+
+@main
+struct MoneyManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
